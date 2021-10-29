688d30728eafabbcfaecd9f175d23609d9f851f7
ScnMatheus <88409537+ScnMatheus@users.noreply.github.com>
2021-10-29 00:03:36 -0300

    atualização dos formulário com segurança e fidelidade ao banco

--- cadastro.php
+++ cadastro.php
@@ -45,13 +45,27 @@
 
         <form action="./Metodos/confirmacaoCadastro.php" method="post">
 
-          <label for="email">E-mail</label>
+         <!-- <label for="email">E-mail</label>
           <input type="email" name="email" id="email" placeholder="Digite seu e-mail" autocomplate="off">
           <label for="password">Senha</label>
           <input type="password" name="senha" id="password" placeholder="Digite a sua senha ">
           <a href="#" id="forgot-pass">Esqueceu a senha?</a>
           <input type="submit" value="Cadastrar">
+        Form anterior 27/10/2021-->
 
+        <!-- criei novos inputs, para acompanhar nosso banco-->
+        <label for="name">Nome Completo</label>
+          <input type="text" name="nome" id="name" placeholder="Digite seu nome completo" autocomplate="off" required="required" size="9" minlength="6" maxlength="30">
+          <label for="email">E-mail</label>
+          <input type="email" name="email" id="email" placeholder="Digite seu e-mail" autocomplate="off" required="required" size="9" minlength="10" maxlength="30">
+          <label for="usuario">Nome de usuario</label>
+          <input type="text" name="nomeusuario" id="usuario" placeholder="Insira seu nome de usuario" required="required" autocomplate="off" size="9" minlength="8" maxlength="30">
+          <label for="password">Senha</label>
+          <input type="password" name="senha" id="password" placeholder="Digite a sua senha "required="required" size="9" minlength="8" maxlength="20">
+          <a href="#" id="forgot-pass">Esqueceu a senha?</a>
+          <input type="submit" value="Cadastrar">
+
+        
         </form>
 
        <div id = "registe-container">
--- login.php
+++ login.php
@@ -41,7 +41,7 @@
 
       <h1>Login</h1>
 
-      <form action="./Metodos/confirmacaoLogin.php" method="post">
+      <!-- <form action="./Metodos/confirmacaoLogin.php" method="post">
 
         <label for="email">E-mail</label>
         <input type="email" name="email" id="email" placeholder="Digite seu e-mail" autocomplate="off">
@@ -49,7 +49,17 @@
         <input type="password" name="senha" id="password" placeholder="Digite a sua senha ">
         <a href="#" id="forgot-pass">Esqueceu a senha?</a>
         <input type="submit" value="Login">
+       FORM ANTIGO 28/10/2021 -->
 
+       <form action="./Metodos/confirmacaoLogin.php" method="post">
+       <label for="email">E-mail</label>
+        <input type="email" name="email" id="email" placeholder="Digite seu e-mail" required="required" size="9" minlength="8" maxlength="30"autocomplate="off">
+        <label for="password">Senha</label>
+        <input type="password" name="senha" id="password" required="required" size="9" minlength="8" maxlength="30" placeholder="Digite a sua senha">
+        <label for="usuario">Nome de usuario:</label>
+        <input type="text" name="usuario" id="usuario" required="required" size="9" minlength="8" maxlength="30" placeholder="Digite seu nome de usuario">
+        <a href="#" id="forgot-pass">Esqueceu a senha?</a>
+        <input type="submit" value="Login">
       </form>
 
        <div id = "registe-container">
